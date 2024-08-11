7c133bd1f33750ef33556627c8279dc6d5870e2d
xoxorbrb <tg3782@naver.com>
2024-08-11 19:29:44 +0900

    로그인 form css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Login/Login";
 import Home from "./Home/Home";
+import "./css/Login/login.css";
+import "./css/button/button.css";
+import "./css/common/common.css";
 function App() {
   return (
     <Router>
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../css/Login/login.css";
 
 const Login = () => {
   const [state, setState] = useState({
@@ -18,36 +17,64 @@ const Login = () => {
       <div className="login-container">
         {state.currentPage === "login" ? (
           <div>
-            <div className="login-card signin">
-              <h2>Login</h2>
-              <form>
-                <input type="text" placeholder="Username" />
-                <input type="password" placeholder="Password" />
-                <button type="submit">Login</button>
-              </form>
-              <button onClick={() => handleCurrentPage()}>Sign</button>
+            <div className={`login-card signin`}>
+              <div className={"box-center"}>
+                <img
+                  className="icon-image"
+                  src="/resource/images/movie_write_icon.png"
+                  width={"100px"}
+                  height={"100px"}
+                  alt=""
+                />
+                <h2>Sign in to Paranoma</h2>
+
+                <form className="form-container">
+                  <span className="left-aligned">
+                    Username or email address
+                  </span>
+                  <input
+                    className="gray-input"
+                    type="text"
+                    placeholder="Username"
+                  />
+                  <input
+                    className="gray-input"
+                    type="password"
+                    placeholder="Password"
+                  />
+                </form>
+                <button
+                  className="text-button gray-text mb20"
+                  onClick={() => handleCurrentPage()}
+                >
+                  Forgot your password?
+                </button>
+                <button className="orange-button" type="submit">
+                  Login
+                </button>
+              </div>
             </div>
 
-            <div
-              className={`login-card-sign-right ${
-                state.currentPage === "login" ? "show" : "hidden"
-              }`}
-            >
-              <div>
-                <h2>Sign Up</h2>
-                <form>
-                  <input type="text" placeholder="Username" />
-                  <input type="email" placeholder="Email" />
-                  <input type="password" placeholder="Password" />
-                  <button type="submit">Sign Up</button>
-                </form>
+            <div className={`login-card-sign-right show`}>
+              <div className="box-center">
+                <h2 className="white-text">Hello, Friend!</h2>
+                <span className="white-text fs12 mb20">
+                  Share your screenplay and let's write a new chapter in film
+                  together.
+                </span>
+                <button
+                  className="write-outline-button"
+                  onClick={() => handleCurrentPage()}
+                >
+                  Signup
+                </button>
               </div>
             </div>
           </div>
         ) : (
           <div>
-            <div className="login-card signup">
-              <h2>Login</h2>
+            <div className={`login-card-sign-left show`}>
+              <h2>ddds</h2>
               <form>
                 <input type="text" placeholder="Username" />
                 <input type="password" placeholder="Password" />
@@ -56,11 +83,7 @@ const Login = () => {
               <button onClick={() => handleCurrentPage()}>Sign</button>
             </div>
 
-            <div
-              className={`login-card-sign-right ${
-                state.currentPage === "login" ? "hidden" : "show"
-              }`}
-            >
+            <div className={`login-card signup`}>
               <div>
                 <h2>dddddd</h2>
                 <form>
