d999f8c7c566f4792e42ca88b843d356f5630ab0
xoxorbrb <tg3782@naver.com>
2024-09-04 23:25:37 +0900

    next.js로 전환

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./Login/Login";
+import Login from "../pages/Login/Login";
 import Home from "./Home/Home";
 import "./css/Login/login.css";
 import "./css/button/button.css";
