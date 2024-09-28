bd19d0739ede480a01267e6941219699fb1d9784
xoxorbrb <tg3782@naver.com>
2024-09-28 23:32:15 +0900

    useDebounce 개선 : 반환되도록 수정

--- src/utils/eventUtil.tsx
+++ src/utils/eventUtil.tsx
@@ -1,15 +1,19 @@
+import { useCallback, useEffect, useRef } from "react";
+
 export function useDebounce<T extends (...args: any[]) => void>( // 어떤 타입의 함수가 와도 실행할 수 있게끔
   func: T,
   delay: number
 ) {
-  let timer: NodeJS.Timeout | null = null;
-
-  return (...args: Parameters<T>) => {
-    if (timer) {
-      clearTimeout(timer);
-    }
-    timer = setTimeout(() => {
-      func(...args);
-    }, delay);
-  };
+  const timer = useRef<NodeJS.Timeout | null>(null);
+  return useCallback(
+    (...args: Parameters<T>) => {
+      if (timer.current) {
+        clearTimeout(timer.current);
+      }
+      timer.current = setTimeout(() => {
+        func(...args);
+      }, delay);
+    },
+    [func, delay]
+  );
 }
